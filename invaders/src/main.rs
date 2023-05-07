14b256585c7b3d0dae580a18a1b2880d977505a0
Diego Giuliani <giuliani.diego@gmail.com>
2023-05-06 21:27:47 -0300

    set up rendering engine

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -4,9 +4,18 @@ use crossterm::{
     terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
 };
+use invaders::{
+    frame::{self, new_frame /* Drawable, Frame */},
+    render,
+};
 
 use rusty_audio::Audio;
-use std::{error::Error, io, time::Duration};
+use std::{
+    error::Error,
+    sync::mpsc::{self /* Receiver */},
+    time::Duration,
+    {io, thread},
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut audio = Audio::new();
@@ -27,8 +36,28 @@ fn main() -> Result<(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?; // Hide the cursor
 
+    // Render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+
+            last_frame = curr_frame;
+        }
+    });
+
     // Game Loop
     'gameloop: loop {
+        // Per-frame init
+        let curr_frame = new_frame();
+
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
@@ -41,9 +70,16 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
+        // Draw and Render
+        let _ = render_tx.send(curr_frame); // Ignoring the error
+
+        thread::sleep(Duration::from_millis(1)); // Prevent generating thoutands of frames per seconds
     }
 
     // Cleanup
+    drop(render_tx); // Drop the tx channel
+    render_handle.join().unwrap(); // Wait until render thread finishes
+
     audio.wait();
     stdout.execute(Show)?; // Show the cursor
     stdout.execute(LeaveAlternateScreen)?;
